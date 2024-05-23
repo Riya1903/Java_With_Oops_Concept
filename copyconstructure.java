8041c373da8f5fec793edad9e2a40bc3e645af22
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-05-23 23:53:29 +0530

    Add files via upload

---
+++ copyconstructure.java
@@ -0,0 +1,52 @@
+public class copyconstructure {
+
+public static void main(String args[])
+{
+    Student s1=new Student();
+    s1.name="riya";
+    s1.roll=12;
+    s1.password="abcd";
+    s1.marks[0]=100;
+    s1.marks[1]=90;
+    s1.marks[2]=80;
+
+    Student s2=new Student(s1);
+    s2.password="xyz";
+    s1.marks[2]=100;
+    for(int i=0; i<3; i++)
+    {
+        System.out.println(s2.marks[i]);
+    }
+
+
+}
+
+}
+class Student{
+    String name;
+    int roll;
+    String password;
+    int marks[];
+    //copy constructure
+    Student(Student s1)
+    {
+        marks=new int[3];
+        this.name=s1.name;
+        this.roll=s1.roll;
+        this.marks=s1.marks;
+    }
+    Student()
+    {
+        marks=new int[3];
+        System.out.println("constructure is callled");
+    }
+    Student(String name)
+    {
+        marks=new int[3];
+        this.name=name;
+    }
+    Student(int roll)
+    {
+        this.roll=roll;
+    }
+}
